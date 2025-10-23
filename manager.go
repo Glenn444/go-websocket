92827ca0cb48bc32f5d481d6fb407733e08cb758
Glenn444 <gmark1586@gmail.com>
2025-10-23 16:38:51 +0300

    Add websocket manager

---
+++ manager.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	WebsocketUpgrader = websocket.Upgrader{
+		ReadBufferSize: 1024,
+		WriteBufferSize: 1024,
+	}
+)
+
+type Manager struct{}
+
+func NewManager() *Manager {
+	return  &Manager{}
+}
+
+
+func (m *Manager)serverWs (w http.ResponseWriter, r *http.Request)  {
+	log.Println("new connection")
+
+	//upgrade connection
+	conn, err := WebsocketUpgrader.Upgrade(w,r,nil)
+	if err != nil{
+		fmt.Printf("error occurred in upgrading socket %v",err)
+		return
+	}
+
+	conn.Close()
+}
